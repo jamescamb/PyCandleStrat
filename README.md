24522e9d9a38a0ee5a0574a42f67f3b8eee96962
cambridge-quant <cummins_james@hotmail.com>
2024-06-15 10:51:56 +0100

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Backtesting Candlestick Patterns 🕯️
+# PyCandleStrat 🕯️
+
+PyCandleStrat is a Python tool that analyses financial candlestick data to build optimal trading strategies.
+It allows the user to specify what candlestick patterns to look for,
+and will return the optimal strategy in terms of buying or selling the following day.
+The tool provides summary statistics on the returns from the strategy.
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 """
-This tool allows the user to specify what candlestick pattern to look for in what context,
-(i.e., bearish engulfing following 2 down days),
-and return the optimal strategy in terms of buy or sell the following days open level, and stop,
-target and take profit for different time horizons following the observation of that pattern,
-along with some summary statistics on the returns from that strategy (i.e., win rate, return, SD of return).
-We assume that so long as a stop or take profit is within the relevant time periods range, it was possible to trade there.
+PyCandleStrat
+
+MIT License 2024
 """
 
 from analysis import Strategy
